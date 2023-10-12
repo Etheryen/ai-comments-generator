61c93e4361669c6ffaf998c2c9dec196c0aae2ef
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-10-12 11:59:59 +0200

    extract layout and add metadata

---
+++ src/components/layout.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import { type PropsWithChildren } from "react";
+
+export interface LayoutProps extends PropsWithChildren {
+  title: string;
+  description: string;
+}
+
+export function Layout({ title, description, children }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-800">
+        {children}
+      </main>
+    </>
+  );
+}

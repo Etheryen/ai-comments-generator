130f5e2adc17fa32ce877bd7badf221963305908
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-11-09 20:20:40 +0100

    add Inter font

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,14 @@
 import Head from "next/head";
 import { type PropsWithChildren } from "react";
+import { Inter } from "next/font/google";
+import { cn } from "~/utils/classnames";
 
 export interface LayoutProps extends PropsWithChildren {
   title: string;
   description: string;
 }
+
+const inter = Inter({ subsets: ["latin"] });
 
 export function Layout({ title, description, children }: LayoutProps) {
   return (
@@ -14,7 +18,12 @@ export function Layout({ title, description, children }: LayoutProps) {
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-800 text-white supports-[height:100dvh]:min-h-[100dvh]">
+      <main
+        className={cn(
+          inter.className,
+          "flex min-h-screen flex-col items-center justify-center bg-neutral-800 text-white supports-[height:100dvh]:min-h-[100dvh]",
+        )}
+      >
         {children}
       </main>
     </>

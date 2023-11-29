fb1706a57bc8f936ff6f2553cac72cddd84a6f45
Etheryen <lukasz.ciwoniuk@gmail.com>
2023-11-29 20:04:11 +0100

    api changes and schema

--- src/server/api/routers/query.ts
+++ src/server/api/routers/query.ts
@@ -4,33 +4,7 @@ import { env } from "~/env.mjs";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { newQuerySchema } from "~/utils/schemas";
 
-const testComments = [
-  {
-    username: "@johndoeDDDDDDDDDDD",
-    message:
-      "Hi there! Welcome to Facebook 🎉 I hope you enjoy exploring all of the features and connecting with friends and family. Let me know if you have any questions or need help getting started! 😊 #FacebookWelcome #NewToSocialMedia",
-  },
-  {
-    username: "@mariagomezDDDDDDDDDDD",
-    message:
-      "Welcome aboard! We are happy to see new users on our page, especially during this challenging time. Don't hesitate to reach out if you need any assistance or have questions about how to use Facebook effectively. 😊 #FacebookCommunity #WelcomeToSocialMedia",
-  },
-  {
-    username: "@sarahjones",
-    message:
-      "Hi there! Welcome to the world of social media 🌐 We hope you find it fun and engaging, and that you connect with people who share your interests and passions. Don't forget to personalize your profile and add a cover photo so you can make an even better impression! 😊 #FacebookWelcome #NewToSocialMedia",
-  },
-  {
-    username: "@markdavis",
-    message:
-      "Welcome to the biggest social network on Earth 🌍 It can be overwhelming at first, but with some experimentation and practice, you'll soon find your groove. Don't forget to join groups that interest you and participate in conversations to build",
-  },
-  {
-    username: "@jessicawuDDDDDDDD",
-    message:
-      "Hello! Welcome to the vast world of social media 🌐 We hope you enjoy connecting with friends, sharing your thoughts and ideas, and discovering new content that inspires you. Remember to be kind and respectful to others, and don't hesitate to reach out if you need help or have questions. 😊 #FacebookWelcome #NewToSocialMedia",
-  },
-];
+const COMMENTS_NUMBER = 3;
 
 const queryResponseSchema = z.array(
   z.object({
@@ -43,22 +17,19 @@ export const queryRouter = createTRPCRouter({
   addNew: protectedProcedure
     .input(newQuerySchema)
     .mutation(async ({ input, ctx }) => {
-      await new Promise((r) => setTimeout(r, 1500));
       try {
-        // const response = await fetch(`${env.API_BASE_URL}/api/comments`, {
-        //   method: "POST",
-        //   body: JSON.stringify({ ...input, commentsNumber: 5 }),
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //     password: env.API_PASSWORD, // TODO: use actual process.env.AI_API_SECRET
-        //   },
-        // });
-        //
-        // const parsedQueryResponse = queryResponseSchema.parse(
-        //   await response.json(),
-        // );
+        const response = await fetch(`${env.API_BASE_URL}/api/comments`, {
+          method: "POST",
+          body: JSON.stringify({ ...input, commentsNumber: COMMENTS_NUMBER }),
+          headers: {
+            "Content-Type": "application/json",
+            password: env.API_PASSWORD, // TODO: use actual process.env.AI_API_SECRET
+          },
+        });
 
-        const parsedQueryResponse = testComments;
+        const parsedQueryResponse = queryResponseSchema.parse(
+          await response.json(),
+        );
 
         const query = await ctx.db.query.create({
           data: {
@@ -108,23 +79,7 @@ export const queryRouter = createTRPCRouter({
   getMoreCommentsToQueryId: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ input, ctx }) => {
-      await new Promise((r) => setTimeout(r, 1500));
       try {
-        // const response = await fetch(`${env.API_BASE_URL}/api/comments`, {
-        //   method: "POST",
-        //   body: JSON.stringify({ ...input, commentsNumber: 5 }),
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //     password: env.API_PASSWORD, // TODO: use actual process.env.AI_API_SECRET
-        //   },
-        // });
-        //
-        // const parsedQueryResponse = queryResponseSchema.parse(
-        //   await response.json(),
-        // );
-
-        const parsedQueryResponse = testComments;
-
         const query = await ctx.db.query.findUnique({
           where: { id: input.id },
           select: {
@@ -137,6 +92,23 @@ export const queryRouter = createTRPCRouter({
 
         if (!query) throw new TRPCError({ code: "NOT_FOUND" });
 
+        const response = await fetch(`${env.API_BASE_URL}/api/comments`, {
+          method: "POST",
+          body: JSON.stringify({
+            query: query.input,
+            type: query.type,
+            register: query.register,
+            commentsNumber: COMMENTS_NUMBER,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            password: env.API_PASSWORD,
+          },
+        });
+
+        const parsedQueryResponse = queryResponseSchema.parse(
+          await response.json(),
+        );
         const comments = await ctx.db.$transaction(
           parsedQueryResponse.map((comment) =>
             ctx.db.comment.create({
